ea40c3cc6f9acc4a885ae2b4791cc3790876f0f0
simpletask go <simpletaskgo@gmail.com>
2025-11-27 14:21:38 +0000

    Try fixing this error: `Runtime Error: Error: useState is not defined. E

--- src/components/ui/process-request-dialog.tsx
+++ src/components/ui/process-request-dialog.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { useMemo, useEffect } from 'react';
+import { useMemo, useEffect, useState } from 'react';
 import { useForm, useFieldArray, type SubmitHandler } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
