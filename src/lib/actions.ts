6592bbdcf7c1130e1fffdfd4bfe04462223bbc24
simpletask go <simpletaskgo@gmail.com>
2025-12-02 14:58:08 +0000

    Configurar URLs de retorno La URL de retorno es la dirección a la que se

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,33 +1,8 @@
 
 'use server';
 
-import fs from 'fs/promises';
-import path from 'path';
-import { revalidatePath } from 'next/cache';
-import { getSettings } from './settings';
-import type { AppSettings } from '@/types/settings';
 import { MercadoPagoConfig, Preference } from 'mercadopago';
 import { redirect } from 'next/navigation';
-
-const settingsFilePath = path.join(
-  process.cwd(),
-  'src/lib/app-settings.json'
-);
-
-// This function IS a Server Action, meant to be called from the client.
-export async function updateSettings(formData: FormData) {
-  const currentSettings = await getSettings();
-
-  const newSettings: AppSettings = {
-    appName: (formData.get('appName') as string) || currentSettings.appName,
-    logoUrl: formData.get('logoUrl') as string,
-  };
-
-  await fs.writeFile(settingsFilePath, JSON.stringify(newSettings, null, 2));
-
-  // Revalidate all paths to reflect the changes immediately
-  revalidatePath('/', 'layout');
-}
 
 export async function createSubscription(prevState: any, formData: FormData) {
   // Configura el cliente de Mercado Pago con tu Access Token.
@@ -51,15 +26,17 @@ export async function createSubscription(prevState: any, formData: FormData) {
           },
         ],
         back_urls: {
-            success: 'https://www.google.com', // URL a la que volver si el pago es exitoso
+          success: 'https://www.google.com', // URL a la que volver si el pago es exitoso
+          failure: 'https://simplestock-two.vercel.app/', // URL de fallo
+          pending: 'https://simplestock-two.vercel.app/', // URL de pago pendiente
         },
         auto_return: 'approved',
       },
     });
 
     // Si se crea el punto de inicio (la URL de pago), redirige al usuario.
-    if (result.init_point) {
-      redirect(result.init_point);
+    if (result.id) {
+      redirect(result.init_point!);
     } else {
       throw new Error('No se pudo obtener el init_point de Mercado Pago');
     }
