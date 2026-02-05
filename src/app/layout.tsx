f673c97b8851cad1a8535c0df958c82220222e91
simpletask go <simpletaskgo@gmail.com>
2026-02-05 12:17:54 +0000

    revisa el ux ui para la version responsive, ya que el ancho de la pagina

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,7 @@ export default async function RootLayout({
   return (
     <html lang="es" suppressHydrationWarning>
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
